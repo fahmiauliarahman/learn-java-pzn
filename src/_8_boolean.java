0d7dd7d3210bc77ec3d14987090c139919f94044
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2025-10-19 19:57:03 +0700

    feat: add basic boolean example in java
    Signed-off-by: Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>

---
+++ src/_8_boolean.java
@@ -0,0 +1,12 @@
+public class _8_boolean {
+
+    public static void main(String[] args) {
+        // boolean is just a datatype that holding the true/false data.
+
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("benar = " + benar);
+        System.out.println("salah = " + salah);
+    }
+}
