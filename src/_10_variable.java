02dc6825b107820c2baf324e52b65e07a63f221b
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2025-10-19 20:04:46 +0700

    feat: add basic variable declaration examples in java
    Signed-off-by: Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>

---
+++ src/_10_variable.java
@@ -0,0 +1,13 @@
+public class _10_variable {
+    public static void main(String[] args) {
+        // variable is a place to store data
+        // there are many ways to create a variable,
+
+        // using var keyword: need a value initializer. example
+        var name = "fahmi"; // automatically the type was String
+
+        // classic method: define data type, name, and value (optional)
+        String firstName; // without value
+        String firstNameAgain = "Fahmi"; // with default value
+    }
+}
