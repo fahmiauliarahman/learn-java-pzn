92c3e9f434ca726d7dba08839a417f6d04e42c8d
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2025-10-19 19:54:21 +0700

    feat: add basic character printing example in Java
    Signed-off-by: Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>

---
+++ src/_7_character.java
@@ -0,0 +1,12 @@
+public class _7_character {
+    public static void main(String[] args) {
+        // char (single character)
+        char F = 'f';
+        char A = 'a';
+        char H = 'h';
+        char M = 'm';
+        char I = 'i';
+
+        System.out.printf("%s%s%s%s%s\n",F,A,H,M,I);
+    }
+}
