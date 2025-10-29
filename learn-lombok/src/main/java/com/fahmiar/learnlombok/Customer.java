10e6fcdd36bb8c6b13326c2f3d20d11ad665d55a
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2025-10-29 14:30:42 +0700

    to string annotation with exclude the other way

--- learn-lombok/src/main/java/com/fahmiar/learnlombok/Customer.java
+++ learn-lombok/src/main/java/com/fahmiar/learnlombok/Customer.java
@@ -5,7 +5,10 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(staticName = "createEmpty") // automatically create a private and static method
 @AllArgsConstructor(staticName = "create")
-@ToString(exclude = "password") // password will not be shown on the result when printing the customer variable
+@ToString(exclude = {
+        "password"
+        // ... other
+}) // password will not be shown on the result when printing the customer variable
 public class Customer {
 
     @Setter(AccessLevel.PROTECTED)
