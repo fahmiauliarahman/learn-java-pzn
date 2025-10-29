b81884a8d93636a01e0cd21228a715973a06ac34
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2025-10-29 14:17:31 +0700

    No args constructor and All Args constructor

--- learn-lombok/src/main/java/com/fahmiar/learnlombok/Customer.java
+++ learn-lombok/src/main/java/com/fahmiar/learnlombok/Customer.java
@@ -1,10 +1,10 @@
 package com.fahmiar.learnlombok;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     @Setter(AccessLevel.PROTECTED) @Getter
